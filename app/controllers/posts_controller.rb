3216f6485cf47be7428e7418cba8a0176b6ff490
Francis Bongiovanni <fbongiovanni29@gmail.com>
2015-09-28 21:57:11 -0400

    First commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+before_filter :authorize, :except => [:index, :show]
+
+def new
+end
+
+def create
+	post = Post.new(post_params.merge(user_id: current_user.id))
+	if post.save
+		redirect_to '/'
+	else
+		redirect_to new_post_path
+	end
+end
+
+def show
+  @post = Post.find(params[:id])
+end
+
+
+def index
+  @posts = Post.all
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
+
+def authorize
+	redirect_to '/login' unless current_user
+end
+
+
+end
+
